3be016d6dbaa8f4124586efc3f6baa9d3771b557
Staffu555 <80742525+Staffu555@users.noreply.github.com>
2021-06-16 17:52:35 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    
+    fill("white")
+    textSize(15)
+    text("A T E D B A S A",100,350);
+    fill("lightblue")
+    text("Hint:ONLINE!! used for multiplayer !!", 100,370);
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N",705,160);
+    fill("lightblue")
+    text("Hint: Can be defined!! are called!!", 705,180);
+
+}
